0a5e6aebabe9543ccadb1c5c0b14f4f421cd7727
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2024-07-24 19:25:49 -0400

    Fixing Deployment Error

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -23,7 +23,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (isLoading) return <Spinner />;
   return (
     <>
-      <Heading fontSize="2xl" marginBottom={3}>
+      <Heading fontSize="2xl" marginBottom={3} marginTop="1px">
         Genres
       </Heading>
       <List>
